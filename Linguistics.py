2a4c1e925f0fd4123a1a66dc95f48ad9029311f3
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2023-05-11 00:44:00 -0700

    Update Linguistics.py

--- Linguistics.py
+++ Linguistics.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 #Defining Parameters
 letterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
